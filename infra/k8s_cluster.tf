9fcdbcb73629f50b62c1aec62d6167f4353f286f
0xV3nn <ogabrielpereira@pm.me>
2025-07-11 14:51:08 -0300

    fix: Corrigindo versão do k8s

--- infra/k8s_cluster.tf
+++ infra/k8s_cluster.tf
@@ -1,7 +1,7 @@
 resource "digitalocean_kubernetes_cluster" "main" {
   name    = "k8s-cluster"
   region  = "nyc1"
-  version = "latest"
+  version = "1.33.1-do.1"
 
   node_pool {
     name       = "default-pool"
